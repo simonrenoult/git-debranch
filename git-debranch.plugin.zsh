cef95b00218c773edfd1d76d6a25bbbc1f00dd02
Simon Renoult <simon.renoult@gmail.com>
2020-06-24 15:24:08 +0200

    Update git-debranch.plugin.zsh

--- git-debranch.plugin.zsh
+++ git-debranch.plugin.zsh
@@ -6,31 +6,41 @@ function debranch {
         local commit_to_extract="$1"
         local branch_to_create="debranch-${commit_to_extract}"
 
+        echo "> Committing current changes in a temporary commit"
         git add --all
-    	git commit --message="wip: undo me with 'git reset --soft HEAD~'"
-        git checkout -b $branch_to_create origin/master
+    	git commit --allow-empty --message="wip: undo me with 'git reset --soft HEAD~'"
+        echo
+        
+        echo "> Moving to the new branch '${branch_to_create}'"
+        git checkout -b $branch_to_create master
+        echo
+        
+        echo "> Cherry-picking '${commit_to_extract}' on '${branch_to_create}'"
         git cherry-pick $commit_to_extract
+        echo
+
+        echo "> Moving back to '${current_branch}'"
         git checkout $current_branch
+        echo
+
+        echo "> Undoing the temporary commit on '${current_branch}'"
         git reset --soft HEAD~
+        echo
+
+        echo "> Branch '${branch_to_create}' has been created, it contains '${commit_to_extract}'"
+        echo
     fi
 }
 compdef _git debranch=git-checkout
 
 function debranch-and-push {
-    if [ -z "$1" ]; then
-        echo "Error: a commit ref must be provided"
-    else
-        local current_branch=$(git rev-parse --abbrev-ref HEAD)
-        local commit_to_extract="$1"
-        local branch_to_create="debranch-${commit_to_extract}"
+    local commit_to_extract="$1"
+    local branch_to_create="debranch-${commit_to_extract}"
+    
+    debranch $commit_to_extract
 
-        git add --all
-        git commit --message="wip: undo me with 'git reset --soft HEAD~'"
-        git checkout -b $branch_to_create origin/master
-        git cherry-pick $commit_to_extract
-        git push origin $branch_to_create
-        git checkout $current_branch
-        git reset --soft HEAD~
-    fi
+    echo "> Pusing '${branch_to_create}' to origin"
+    git push origin $branch_to_create
+    echo
 }
 compdef _git debranch-and-push=git-checkout
